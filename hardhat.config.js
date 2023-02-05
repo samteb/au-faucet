7df336f57ad7fb279c3b684d49987de8d8a0696a
samuel <sam.teboul@gmail.com>
2023-02-05 09:42:15 +0100

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
+};
